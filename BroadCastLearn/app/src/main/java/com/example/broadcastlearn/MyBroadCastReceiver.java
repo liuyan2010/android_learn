a58e2b7be6c83c71c2d46b22850406dcd148706b
yanliu <ly2010jlu13@163.com>
2020-12-17 15:47:28 +0800

    BroadCastReceiever 示例操作
    Signed-off-by: yanliu <ly2010jlu13@163.com>

---
+++ BroadCastLearn/app/src/main/java/com/example/broadcastlearn/MyBroadCastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.broadcastlearn;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadCastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "I recieve it,haha",Toast.LENGTH_SHORT).show();
+        abortBroadcast();
+    }
+}
